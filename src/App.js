01d149f6157f3525a75ae82ab35b96988db4c09e
Soumyanetrap <soumyanetrap@gmail.com>
2024-08-30 20:48:46 +0530

    deploy req

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-// App.js
-
 import React from 'react';
-
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Home from './components/Home'; // Make sure to adjust the path if necessary
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
 import Dashboard from './components/Dashboard/Dashboard';
 import Finance from './components/Finance/Finance';
 import Banks from './components/Banks/Banks';
-
-
-import './App.css'; // Import the CSS file for styling
 import AddAccount from './components/Add_Accounts/Add_Accounts';
 import Tickets from './components/Tickets/Tickets';
 import RaiseTickets from './components/Raise_Tickets/RaiseTickets';
 import Connections from './components/Connections/Connections';
 import PendingRequests from './components/Pending_Requests/PendingRequests';
-
+import './App.css'; 
 
 const App = () => {
   return (
@@ -24,17 +18,17 @@ const App = () => {
         <h1>All in 1</h1>
       </header>
       <main className="App-Main">
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/finance" element={<Finance />} />
-              <Route path="/banks" element={<Banks />} />
-              <Route path="/connect" element={<Connections />} />
-              <Route path="/add_ac" element={<AddAccount />} />
-              <Route path="/tickets" element={<Tickets />} />
-              <Route path="/iraise" element={<RaiseTickets />} />
-              <Route path="/ipending" element={<PendingRequests />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/finance" element={<Finance />} />
+            <Route path="/banks" element={<Banks />} />
+            <Route path="/connect" element={<Connections />} />
+            <Route path="/add_ac" element={<AddAccount />} />
+            <Route path="/tickets" element={<Tickets />} />
+            <Route path="/iraise" element={<RaiseTickets />} />
+            <Route path="/ipending" element={<PendingRequests />} />
           </Routes>
         </Router>
       </main>
